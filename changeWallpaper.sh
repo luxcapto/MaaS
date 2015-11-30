de3eb3cd6f26560f2d382ff6639b5e0287b4d5ad
Rytis Bizauskas <luxcapto@gmail.com>
2015-11-30 00:51:48 -0600

    Improved comments on script

--- changeWallpaper.sh
+++ changeWallpaper.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-user=YOUR_USER_NAME
+//The DBUS lines are required for the cron job to run properly.
+//If you get errors, check the output of pgrep gnome-session.
+//If you get more than one session, then you'll have to use tail or head to make sure the right session is saved to PID and sessionFile.
 
-//tail -n 1 needed if pgrep gnome-session returns more than one line
-//i had multiiple dbus sessions going and session 1 was the right one
-
-PID=$(pgrep gnome-session | tail -n 1) 
+PID=$(pgrep gnome-session) 
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
-//same deal as above with the tail -n 1
-
-sessionfile=`find "${HOME}/.dbus/session-bus/" -type f | tail -n 1`
+sessionfile=`find "${HOME}/.dbus/session-bus/" -type f`
 echo $sessionFile
 export `grep "DBUS_SESSION_BUS_ADDRESS" "$sessionFile" | sed '/^#/d'`	
 
-//your user name and/or directory to where you will store your wallpapaers
+//Your user name and/or directory to where you will store your wallpapaers
+cd /home/your-username-here/Pictures/Wallpapers
 
-cd /home/$user/Pictures/Wallpapers
-
-
-//pulls down all the memes
+//Pulls down all the memes
 wget -q -o /dev/null -r -A jpg -nd -N -k http://memecdn.net/images/
 
-//picks the freshest and dankest meme
-
+//Picks the freshest and dankest meme
 desktopImage=$(ls -Art | tail -n 1)
 
-//sets desktop for gnome desktops
-imageRoot='file:///home/$user/Pictures/Wallpapers/'
+//Sets desktop for gnome desktops
+imageRoot='file:///home/your-username-here/Pictures/Wallpapers/'
 fullURI=$imageRoot$desktopImage
 gsettings set org.gnome.desktop.background picture-uri $fullURI 
