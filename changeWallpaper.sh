3c1f3873e77c52cdc5dc9a1e47ae6413f193f57d
Rytis Bizauskas <luxcapto@gmail.com>
2015-11-30 00:53:10 -0600

    Fixed comment tags on script

--- changeWallpaper.sh
+++ changeWallpaper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-//The DBUS lines are required for the cron job to run properly.
-//If you get errors, check the output of pgrep gnome-session.
-//If you get more than one session, then you'll have to use tail or head to make sure the right session is saved to PID and sessionFile.
+#The DBUS lines are required for the cron job to run properly.
+#If you get errors, check the output of pgrep gnome-session.
+#If you get more than one session, then you'll have to use tail or head to make sure the right session is saved to PID and sessionFile.
 
 PID=$(pgrep gnome-session) 
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
@@ -11,16 +11,16 @@ sessionfile=`find "${HOME}/.dbus/session-bus/" -type f`
 echo $sessionFile
 export `grep "DBUS_SESSION_BUS_ADDRESS" "$sessionFile" | sed '/^#/d'`	
 
-//Your user name and/or directory to where you will store your wallpapaers
+#Your user name and/or directory to where you will store your wallpapaers
 cd /home/your-username-here/Pictures/Wallpapers
 
-//Pulls down all the memes
+#Pulls down all the memes
 wget -q -o /dev/null -r -A jpg -nd -N -k http://memecdn.net/images/
 
-//Picks the freshest and dankest meme
+#Picks the freshest and dankest meme
 desktopImage=$(ls -Art | tail -n 1)
 
-//Sets desktop for gnome desktops
+#Sets desktop for gnome desktops
 imageRoot='file:///home/your-username-here/Pictures/Wallpapers/'
 fullURI=$imageRoot$desktopImage
 gsettings set org.gnome.desktop.background picture-uri $fullURI 
